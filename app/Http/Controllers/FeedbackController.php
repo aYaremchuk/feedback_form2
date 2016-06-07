d7ae2b83d463477893aba5ef2b8a36b7018f8260
Yaremchuk <jaremchuk.andrij@gmail.com>
2016-06-08 00:32:21 +0300

    remove unneeded from feedbackcontroller

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -64,27 +64,6 @@ class FeedbackController extends Controller
         $feedback = Feedback::find($id);
         return view('feedbacks.show',compact('feedback'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
     /**
      * Remove the specified resource from storage.
      *
